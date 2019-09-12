5ac0f9a72a00bcdc24f7313e3c19009df9c17ae4
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-12 19:21:38 +0100

    WIP: initial commit, setup migrations

---
+++ data/migrations/20190912183606_first.js
@@ -0,0 +1,53 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('zoos', tbl => {
+      tbl.increments();
+
+      tbl.string('zoo_name', 255).notNullable();
+      tbl.string('address').unique();
+    })
+    .createTable('species', tbl => {
+      tbl.increments();
+      tbl.string('species_name', 512).unique();
+    })
+    .createTable('animals', tbl => {
+      tbl.increments();
+
+      tbl.string('animal_name', 255).notNullable();
+
+      // Foreign Key
+      tbl
+        .integer('species_id')
+        .unsigned()
+        .references('id')
+        .inTable('species')
+        .onDelete('CASCADE') // if the PK record is deleted
+        .onUpdate('CASCADE'); // if the PK value updates
+    })
+    .createTable('zoo_animals', tbl => {
+      tbl
+        .integer('zoo_id')
+        .unsigned()
+        .references('id')
+        .inTable('zoos')
+        .onDelete('CASCADE') // if the PK record is deleted
+        .onUpdate('CASCADE'); // if the PK value updates
+      tbl
+        .integer('animal_id')
+        .unsigned()
+        .references('id')
+        .inTable('animals')
+        .onDelete('CASCADE') // if the PK record is deleted
+        .onUpdate('CASCADE'); // if the PK value updates
+
+      tbl.primary(['zoo_id', 'animal_id']);
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('zoo_animals')
+    .dropTableIfExists('animals')
+    .dropTableIfExists('species')
+    .dropTableIfExists('zoos');
+};
--- notes.md
+++ notes.md
@@ -1,8 +1,37 @@
-# Requirements
+# Notes
 
-A client has hired you to track zoo animals.
-For each individual animal, you must track that their name, species, and all zoos in which they have resided (including zoo name and address).
+## A good data model
 
-Determine the database tables necessary to track this information.
+- captures ALL the information the system needs
+- captures ONLY the information the system needs <== _Abstraction_
+- reflects reality (from the point of view of the system)
+- is flexible, can evolve with the system
+- guarantess data integrity, without sacrificing performance <-- _using constraints_
+- is driven by the way we access the data
 
-Label any relationships between table.
+## Components
+
+- entities (nouns: zoo, animal, species), like a resource --> tables
+- properties --> columns or fields.
+- relationships --> Foreign Keys
+
+## Workflow
+
+- identify entities 
+- identify properties
+- identify relationships
+
+## Relationships
+
+- one to one
+- one to many
+- many to many
+
+## Mantras
+
+- every table must have a Primary Key
+- one to many relationship requires a Foreign key
+- the Foreign key goes on the many side
+- many to many needs a third table
+- the third table can have other information
+- work on two or three entities at a time
